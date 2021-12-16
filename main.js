e038bcfc61ef7fc970f81efc3e9960c1e5db9212
AmteurPotion <1k0j2i4@gmail.com>
2021-12-16 15:28:36 +0900

    main

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-() => { return 1; }
+(...args) => {
+    pass = true;
+    securityCheck = (arg) => {
+        return true;
+    };
+
+    for(let i = 0, n = args.length; i < n; i++) {
+        if(pass) {
+            pass = securityCheck(args[i])
+        } else {
+            break;
+        }
+    }
+
+    if(pass) {
+        msg.reply(args);
+    } else {
+        msg.reply("security false arguments...");
+    }
+}
