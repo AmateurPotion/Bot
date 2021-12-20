12d4df190627ebaf77c4240a2252c91c5043f46a
AmteurPotion <1k0j2i4@gmail.com>
2021-12-20 18:54:47 +0900

    Main Codes

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-(...args) => {
+function Run() {
     pass = true;
     securityCheck = (arg) => {
         return true;
     };
 
-    for(let i = 0, n = args.length; i < n; i++) {
+    for(let i = 0, n = arguments.length; i < n; i++) {
         if(pass) {
-            pass = securityCheck(args[i])
+            pass = securityCheck(arguments[i])
         } else {
             break;
         }
     }
 
     if(pass) {
-        msg.reply(args);
+        msg.reply(arguments);
     } else {
         msg.reply("security false arguments...");
     }
